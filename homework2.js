ae643be6936541d841dd50a4f7d6dbc4b9b3ecbd
susieavagyan <43077132+susieavagyan@users.noreply.github.com>
2018-09-12 20:51:34 +0400

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -1,22 +1,23 @@
+alert ("Hi AUA Undergrad Class of 2k22 ")
 
 const PROGRAM="data science";
 const COURSENumber=39;
 const PINPUT="computer science";
 const CINPUT=40;
 
-if (PINPUT==="data science" && CINPUT===39){
-console.log ("Congrats!You are registered for the Data Science Program.")
-} else if (PINPUT=== "computer science" && CINPUT===40){
-console.log ("Congrats!You are registered for the Computer Science Program.") 
-} else if(PINPUT=== "engineering sciences" && CINPUT===45) {
-console.log ("Congrats!You are registered for the Engineering Sciences Program.")
-} else {
-console.log ("Incorrect program and/or course number")
-}
-console.log ("The following is how much more courses you take from the Data Science program students.")
+ if (PINPUT==="data science" && CINPUT===39){
+ console.log ("Congrats!You are registered for the Data Science Program.")
+ } else if (PINPUT=== "computer science" && CINPUT===40){
+ console.log ("Congrats!You are registered for the Computer Science Program.") 
+ } else if(PINPUT=== "engineering sciences" && CINPUT===45) {
+ console.log ("Congrats!You are registered for the Engineering Sciences Program.")
+ } else {
+ console.log ("Incorrect program and/or course number")
+ }
+ console.log ("The following is how much more courses you take from the Data Science program students.")
  if ( CINPUT>COURSENumber){
-console.log (CINPUT-COURSENumber)
-}
+ console.log (CINPUT-COURSENumber)
+ }
 
 
 
